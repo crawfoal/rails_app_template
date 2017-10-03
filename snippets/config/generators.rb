92ad64ce3999939d205f8cfc676761aa457b6bb8
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-10-02 20:21:25 -0400

    Fix indentation in application.rb

--- snippets/config/generators.rb
+++ snippets/config/generators.rb
@@ -1,7 +1,7 @@
 config.generators do |g|
-  g.stylesheets false
-  g.javascripts false
-  g.helper false
-  g.view_specs false
-  g.jbuilder false
-end
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.view_specs false
+      g.jbuilder false
+    end
