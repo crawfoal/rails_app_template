19c4a9fe5d145e4c48ddf8aea07dfeed2ba7cff9
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-10-03 13:21:06 -0400

    Clean up `PageObject` code
    Simplify initialization logic and add details to error messages

--- templates/spec/page_objects/base.rb
+++ templates/spec/page_objects/base.rb
@@ -3,32 +3,9 @@ module PageObjects
     include Capybara::DSL
     include AbstractController::Translation
 
-    # Whenever a new page object is defined, add an accessor method for it to
-    # to the main module. This method is named after the page object's class. It
-    # is responsible for either grabbing the existing page object, or creating a
-    # new one, as well as ensuring that the object is currently on the page.
-    #
-    # rubocop:disable Metrics/MethodLength
-    def self.inherited(subclass)
-      po_name = subclass.name.underscore.split('/').last
-      int_method_nm = "_#{po_name}_"
-
-      PageObjects.module_eval do
-        define_method po_name do
-          page_object = send(int_method_nm)
-          raise NotFound.new(po_name) unless page_object.on_page?
-          return page_object
-        end
-      end
-
-      PageObjects.module_eval do
-        define_method int_method_nm do
-          instance_variable_get("@#{po_name}") ||
-            instance_variable_set("@#{po_name}", subclass.new)
-        end
-      end
+    def initialize(skip_error = false)
+      raise NotFound.new page, self.class.name unless on_page? || skip_error
     end
-    # rubocop:enable Metrics/MethodLength
 
     def on_page?(options = {})
       has_css?(selector, options)
@@ -42,17 +19,6 @@ module PageObjects
 
     def this
       find(selector)
-    end
-  end
-
-  class NotFound < StandardError
-    def initialize(po_name = nil)
-      details = " (#{po_name})" unless po_name.blank?
-      msg = <<~TEXT
-        The page object#{details} wasn't on the page when the object was
-        accessed.
-      TEXT
-      super(msg)
     end
   end
 end
